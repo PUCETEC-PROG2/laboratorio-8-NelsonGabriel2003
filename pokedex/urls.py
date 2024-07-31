98bb6fbdbf78de6b89cae5a5e140c751c87e875e
Nelson Gabriel <ngallauca@puce.edu.ec>
2024-07-31 17:40:12 -0500

    Cuarto Commit

--- pokedex/urls.py
+++ pokedex/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'pokedex'
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("<str:pokemon>/", views.pokemon, name="pokemon"),
